d7a142394b41d5cad8a13af521f690393596aaf3
rezanikafarin1937 <rezanikafarin700@gmail.com>
2026-08-01 01:41:25 -0700

    Performing CRUD operations on a MySQL database using Node.js

--- src/mysql_database.js
+++ src/mysql_database.js
@@ -9,8 +9,86 @@ const pool = mysql
   })
   .promise();
 
-const getCities = async () => {
-  const result = await pool.query("select * from cities");
-  console.log("result = ", result);
-};
-getCities()
+
+
+/* read all data */  
+// const getCities = async () => {
+//   const result = await pool.query("select * from cities");
+//   return result;
+// };
+// var data = []
+// getCities()
+//   .then((res) => {
+
+//     [...data] = res;
+//     console.log("data first log = ", data);
+//   })
+//   .then(() => {
+//     console.log("data second log = ", data);
+//   }).catch(err => {
+//     console.log(err)
+//   });
+
+
+
+
+
+//read one data
+  let city = null;
+  const getCity = async (id)=>{
+    const d = await pool.query(`select * from cities where id = ?`,[id])
+    city = [...d]
+    console.log('city = ',city[0][0])
+    // این روش هم درست است
+    // city = d[0][0]
+    // console.log('city = ',city)
+
+  }
+//   getCity(69)
+
+
+
+// insert data in cities table
+// const insertCity = async (id,provinceId,city) => {
+//     const result = await pool.query(`insert into cities (id,province_id,name) values (?,?,?)` , [id,provinceId,city])
+//     return result;
+// }
+
+// const data = insertCity(1001,33,'مهرشهر').then(result => {
+//     console.log('new city = ',result)
+// }).catch(err=>{
+//     console.log(err)
+// })
+
+// getCity(1000)
+
+
+// update 
+// const updateCity = async (id,city) =>{
+//    const [result] =  await pool.query('update cities set name = ?  where id = ?',[city,id])
+//    return getCity(id)
+// }
+// const newCity =  updateCity(1001,'شهرعلی')
+// console.log('update is = ',newCity)
+
+
+
+// delete
+const deleteCity = async (id) =>{
+    const result = pool.query('delete from cities where id = ?',[id]);
+    if(result){
+        return true
+    }
+    else{
+        false
+    }
+}
+const isDelete = deleteCity(1001);
+if(isDelete){
+    console.log('record is deleted')
+}
+else
+{
+    console.log('not found')
+}
+
